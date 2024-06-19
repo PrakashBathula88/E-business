01a61c59a63d7e1e4e79a9532e0e223c6be78435
BATHULA PRAKASH <152945490+PrakashBathula88@users.noreply.github.com>
2024-06-19 13:48:25 +0530

    Add files via upload

---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { useRef, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import "../Profile/Profile.css";
+
+import Authcontext from "../LoginProvider/Loginprovider";
+const Profile = () => {
+  const navigate=useNavigate();
+  const newpasswordInputref = useRef();
+
+  const authCtx = useContext(Authcontext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredNewpassword = newpasswordInputref.current.value;
+      
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAOD-cVUQG4pIYE1uRNv-KFcDQFvXv7JVU",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idtoken: authCtx.token,
+          password: enteredNewpassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        }
+      }).then(res =>{
+      navigate('/')
+    });
+  };
+
+  return (
+    <div className="form-container">
+    <div className="logo-container">Forgot Password</div>
+
+    <form className="form" onSubmit={submitHandler}>
+      <div className="form-group">
+        <label htmlFor="newPassword">New Password</label>
+        <input
+          type="password"
+          id="newPassword"
+          minLength="8"
+      ref={newpasswordInputref}
+          required
+        />
+      </div>
+
+      <button className="form-submit-btn" type="submit">
+        Send Email
+      </button>
+    </form>
+
+    <p className="signup-link">
+      Don't have an account?{" "}
+      <a href="Loding......" className="signup-link link">
+        Sign up now
+      </a>
+    </p>
+  </div>
+  );
+};
+
+export default Profile;
+
